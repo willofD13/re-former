ad0d65b194e0f341b7d3347a5273089695242a01
willofD13 <leonidastsiakas13@gmail.com>
2023-11-10 20:41:33 +0200

    Display the user after an update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
 
         if @user.update(user_params)
-            redirect_to new_user_path
+            redirect_to user_path(@user.id)
         else
             render :edit, status: :unprocessable_entity
         end
